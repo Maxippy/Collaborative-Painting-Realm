e3d8aa1a645088323152b56f3e97117cd0b28f5a
Maxippy <129250549+Maxippy@users.noreply.github.com>
2024-05-29 18:42:08 +0800

    Update org-sidebar.tsx

--- app/(dashboard)/_components/org-sidebar.tsx
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -58,7 +58,7 @@ export const OrgSidebar = () => {
         >
           <Link href="/">
             <LayoutDashboard className="h-4 w-4 mr-2" />
-            Team Boards
+            团队画板
           </Link>
         </Button>
         <Button
